c9660c4049df368c3780ee3bf7dcdf1b134f1aac
Alekhya Mandala <alekhya.mandala99@gmail.com>
2020-06-23 00:28:28 +0530

    Updates

---
+++ precache-manifest.b78c94724c3eae8ead4c161e9652b5cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579fa7aaeff86deb4ef0ac2938eb6aee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cfbd79f8df08b8a3c423",
+    "url": "./static/css/main.ab003eb6.chunk.css"
+  },
+  {
+    "revision": "7ea7fa3654f44c214f5f",
+    "url": "./static/js/2.80fda868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.80fda868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfbd79f8df08b8a3c423",
+    "url": "./static/js/main.f9a251b6.chunk.js"
+  },
+  {
+    "revision": "b793c38fc953fa14b65d",
+    "url": "./static/js/runtime-main.208b55f4.js"
+  },
+  {
+    "revision": "4948508ccfd3552d72bc0783dfe5502f",
+    "url": "./static/media/delivery-man.4948508c.svg"
+  }
+]);
